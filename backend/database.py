a028a75a8f3f748c300f78fd1631dd861d2e535f
NguyenTAnh2005 <23050118@student.bdu.edu.vn>
2025-11-09 06:07:11 +0700

    update backend, day-1, learning day by day

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # 1. "Địa chỉ" CSDL 
 #    Định dạng là: "mysql+DRIVER://USER:PASSWORD@HOST/DATABASE"
-SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://user1:mysqluser1@localhost/test"
+SQLALCHEMY_DATABASE_URL=os.getenv("SQLALCHEMY_DATABASE_URL")
 
 # 2. Tạo engine kết nối tới CSDL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
